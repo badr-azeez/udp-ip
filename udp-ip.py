8817e1f01b1dc659fe7583faca03a89cba26dd0d
Badr Azeez <34779263+badr-azeez@users.noreply.github.com>
2020-01-21 20:22:32 +0300

    Add files via upload

---
+++ udp-ip.py
@@ -0,0 +1,28 @@
+import pyshark, netifaces
+
+print('|#| udp-ip |#|\n|#| By: Badr Azeez |#| ')
+
+srcips = []  # stored packet
+interface = netifaces.gateways()['default'][netifaces.AF_INET][1]  # interface
+dest = netifaces.ifaddresses(interface)[netifaces.AF_INET][0]['addr']  # ip address this lab
+gws = netifaces.gateways()
+gw = gws['default'][2][0]
+try:
+    # get packet
+    cap = pyshark.LiveCapture('wlan0', bpf_filter='udp')
+    for packet in cap.sniff_continuously(packet_count=15):
+        ip = packet['ip'].src
+        srcips.append(ip)
+    cap.close()
+except PermissionError:
+    exit('run as root')
+finally:
+    cap.run_until_complete(loop.shutdown_asyncgens())
+    cap.close()
+
+if len(srcips) > 0:
+    for ip in set(srcips):
+        if ip != dest and ip != gw:
+            print('ip:', ip)
+else:
+    exit('Not Found udp packet')
